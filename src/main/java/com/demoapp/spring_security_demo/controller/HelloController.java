1c7c774d86e58355c463515a26b30ae0c6a085b8
Ruvindu Madhushanka <ruvindu.madhushanka@directfn.com>
2024-01-17 16:09:15 +0530

    Hello text added

---
+++ src/main/java/com/demoapp/spring_security_demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.demoapp.spring_security_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
