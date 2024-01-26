37d0deff190dc8c91ade19afb5e68a835e099aa7
Ruvindu Madhushanka <ruvindu.madhushanka@directfn.com>
2024-01-26 17:04:56 +0530

    Authentication and authorize added

--- src/main/java/com/demoapp/spring_security_demo/controller/HelloController.java
+++ src/main/java/com/demoapp/spring_security_demo/controller/HelloController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/hi")
+    public String hi() {
+        return "hi!";
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello!";
     }
+
 }
